32b2d1baabe6c67b47485fd3af0c4816a58b632a
Santi D <santid13@hotmail.com>
2022-02-16 20:46:56 +0000

    finished array exercises

---
+++ arrays/are_we_same.rb
@@ -0,0 +1,8 @@
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+p array1 == array2 ? true : false 
+
+#OR
+
+puts array1 == array2
---
+++ arrays/doubled.rb
@@ -0,0 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+
+double_numbers = numbers.map {|n|  n * 2} 
+
+p double_numbers 
